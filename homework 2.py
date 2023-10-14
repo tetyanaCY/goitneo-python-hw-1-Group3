d3b4b640e940147cbed8cd98ab5654baf55a1385
tetyanaCY <t.andreades@yahoo.com>
2023-10-14 11:46:52 +0300

    Add files via upload

---
+++ homework 2.py
@@ -0,0 +1,62 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
+
+def add_contact(args, contacts):
+    if len(args) != 2:
+        return "Invalid format. Correct format: add username phone"
+    
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def change_contact(args, contacts):
+    if len(args) != 2:
+        return "Invalid format. Correct format: change username phone"
+    
+    name, phone = args
+    if name not in contacts:
+        return f"No contact named {name}."
+    
+    contacts[name] = phone
+    return "Contact updated."
+
+def get_phone(args, contacts):
+    if len(args) != 1:
+        return "Invalid format. Correct format: phone username"
+    
+    name = args[0]
+    return contacts.get(name, f"No contact named {name}.")
+
+def show_all_contacts(contacts):
+    if not contacts:
+        return "No contacts saved."
+    
+    return '\n'.join([f"{name}: {phone}" for name, phone in contacts.items()])
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(get_phone(args, contacts))
+        elif command == "all":
+            print(show_all_contacts(contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
